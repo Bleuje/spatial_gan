8d46e0d416ee467700868c98017c764e50f9cc02
E. Jacob <etin.jacob@gmail.com>
2019-03-17 14:04:15 +0100

    changed training data

--- config.py
+++ config.py
@@ -48,9 +48,9 @@ class Config(object):
 
     ##
     # data input folder
-    sub_name    = 'efros2'
+    sub_name    = 'michaux'
     home        = os.path.expanduser("~")
-    texture_dir = home + "/my_data/%s/" % sub_name
+    texture_dir = "/my_data/%s/" % sub_name
     data_iter   = get_texture_iter(texture_dir, npx=npx, mirror=False, batch_size=batch_size)
 
     save_name   = sub_name+ "_filters%d_npx%d_%dgL_%ddL" % (dis_fn[0],npx,gen_ls, dis_ls)
